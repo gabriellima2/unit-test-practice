552ece825e86ff7ea3847e7776fa466f31fe536a
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-08-25 17:45:36 -0300

    test: adiciona teste para Input do Form

--- src/components/Form/Form.spec.tsx
+++ src/components/Form/Form.spec.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/dom";
+import { fireEvent, screen } from "@testing-library/dom";
 import "@testing-library/jest-dom";
 
 import { renderWithProviders } from "../../utils/test-utils";
@@ -9,14 +9,30 @@ import { Form } from ".";
 
 describe("Form Component", () => {
   const store = setupStore();
-  const handleSubmit = jest.fn(() => store.dispatch(addUser("Gabriel")));
+  const handleSubmit = jest.fn();
+
+  let usernameInput: HTMLInputElement;
+  let submitButton: HTMLButtonElement;
 
   beforeAll(() => {
     renderWithProviders(<Form handleSubmit={handleSubmit} />);
-  })
+  });
 
   it ("should render", () => {
-    expect(screen.getByDisplayValue("")).toHaveAttribute("id", "username");
-    expect(screen.getByText("Adicionar")).toBeInTheDocument();
-  })
-})
+    usernameInput = screen.getByDisplayValue("");
+    submitButton = screen.getByText("Adicionar");
+
+    expect(usernameInput).toHaveAttribute("id", "username");
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  describe("User interactions in Form", () => {
+    it("should show the username entered", () => {
+      fireEvent.change(usernameInput, {target: {value: "Gabriel"}});
+
+      expect(usernameInput).toHaveValue("Gabriel");
+    });
+
+    it("should call the submit function", () => {})
+  });
+});
